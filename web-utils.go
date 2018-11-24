ba327d0b22324dec54c8c115af487e0585cc6eff
toddgator <toddgator@gmail.com>
2018-11-24 13:31:19 -0500

    Renaming DownloadFile to DownloadFileWithCounter

--- web-utils.go
+++ web-utils.go
@@ -9,10 +9,10 @@ import (
 	"strings"
 )
 
-// DownloadFile will download a url to a local file. It's efficient because it will
+// DownloadFileWithCounter will download a url to a local file. It's efficient because it will
 // write as it downloads and not load the whole file into memory. We pass an io.TeeReader
 // into Copy() to report progress on the download.
-func DownloadFile(filepath string, url string) error {
+func DownloadFileWithCounter(filepath string, url string) error {
 
 	// Create the file, but give it a tmp file extension, this means we won't overwrite a
 	// file until it's downloaded, but we'll remove the tmp extension once downloaded.
